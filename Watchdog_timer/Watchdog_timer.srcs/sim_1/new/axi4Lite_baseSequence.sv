63e064c8d8dd092f56fa8f300123360e585b7ed3
Constantin Denis <denis.io.constantin@gmail.com>
2024-12-23 20:25:57 +0200

    Update axi4Lite_baseSequence.sv

--- Watchdog_timer/Watchdog_timer.srcs/sim_1/new/axi4Lite_baseSequence.sv
+++ Watchdog_timer/Watchdog_timer.srcs/sim_1/new/axi4Lite_baseSequence.sv
@@ -4,8 +4,8 @@ import uvm_pkg::*;
 class axi4Lite_baseSequence extends uvm_sequence #(axi4Lite_transaction);
     `uvm_object_utils(axi4Lite_baseSequence)
     
-    int transactionAmount = 10;
-    axi4Lite_transaction item;
+    int transactionAmount;
+    axi4Lite_transaction axi4Lite_item;
     
     function new(string name="axi4Lite_baseSequence");
         super.new(name);
@@ -13,23 +13,18 @@ class axi4Lite_baseSequence extends uvm_sequence #(axi4Lite_transaction);
     
     virtual task body();
         
-        item = axi4Lite_transaction::type_id::create("item"); // numele il alegem noi, merge doar daca am uvm factory, line 5
+        axi4Lite_item = axi4Lite_transaction::type_id::create("axi4Lite_item"); // numele il alegem noi, merge doar daca am uvm factory, line 5
         
         `uvm_info("axi4Lite_baseSequence", $psprintf("Going to drive %d items", transactionAmount), UVM_NONE)
         repeat(transactionAmount) begin
-        start_item(item);
-       /* 
-        item.writeEnable = 1;
-        item.s_axi_araddr = 5;
-        item.s_axi_wdata = 10;*/
+            start_item(axi4Lite_item);
+             
+            axi4Lite_item.randomize();
         
-        // or
-        
-        item.randomize();
-        
-        finish_item(item); //blocheaza executia pana cand am terminat cu secventele
+            finish_item(axi4Lite_item); //blocheaza executia pana cand am terminat cu secventele
         
         end
+        `uvm_info("axi4Lite_baseSequence", $psprintf("Finished generating axi4Lite transactions"), UVM_NONE)
     endtask : body
     
 endclass : axi4Lite_baseSequence
