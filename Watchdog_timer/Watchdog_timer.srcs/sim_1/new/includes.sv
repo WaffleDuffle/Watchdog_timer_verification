4cda7f0bf5e1c358389e98953a15aebc6cbcf348
Constantin Denis <denis.io.constantin@gmail.com>
2024-12-23 20:28:15 +0200

    Add files via upload

---
+++ Watchdog_timer/Watchdog_timer.srcs/sim_1/new/includes.sv
@@ -0,0 +1,9 @@
+
+`include "axi4Lite_intf.sv"
+`include "axi4Lite_transaction.sv"
+`include "axi4Lite_monitor.sv"
+`include "axi4Lite_driver.sv"
+`include "axi4Lite_agent.sv"
+`include "axi4Lite_baseSequence.sv"
+`include "environment.sv"
+`include "base_test.sv"
