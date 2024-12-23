a99f0f6584730273da49f2004688eddd6c62c1e9
Constantin Denis <denis.io.constantin@gmail.com>
2024-12-23 20:26:31 +0200

    Update base_test.sv

--- Watchdog_timer/Watchdog_timer.srcs/sim_1/new/base_test.sv
+++ Watchdog_timer/Watchdog_timer.srcs/sim_1/new/base_test.sv
@@ -24,19 +24,33 @@ class base_test extends uvm_test;
     
     virtual task run_phase(uvm_phase phase);
         virtual axi4Lite_intf axi4Lite;
-        
+        axi4Lite_baseSequence baseSeq;
         uvm_config_db#(virtual axi4Lite_intf)::get(null, "", "axi4Lite_interface", axi4Lite);
 
-        phase.raise_objection(this);        
+        baseSeq = axi4Lite_baseSequence::type_id::create("baseSeq");
+        phase.raise_objection(this);   
         
-        // 'main' function of this component
+        // reset the module
+        axi4Lite.s_axi_aresetn = 0;
+        repeat(5) @(posedge axi4Lite.s_axi_aclk);
+        axi4Lite.s_axi_aresetn = 1;
+        
+        
+        baseSeq.transactionAmount = 15;
+             
+        baseSeq.start(env.agent.sequencer);
+        phase.drop_objection(this);
+        
+        
+    /*    // 'main' function of this component
         `uvm_info("base_test", "This is a message", UVM_NONE)
         `uvm_info("base_test", "This is another message", UVM_NONE)
 
         `uvm_info("base_test", "Waiting for 100 clk cycles...", UVM_NONE)
         repeat(100) @(posedge axi4Lite.s_axi_aclk);
         `uvm_info("base_test", "Waiting for 100 clk cycles... DONE", UVM_NONE)
+        */
+           
         
-        phase.drop_objection(this);
     endtask
 endclass
