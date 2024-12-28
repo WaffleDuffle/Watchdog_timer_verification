cb841eca37cf1e14a94486adf471ebe35b8d44ff
Constantin Denis <denis.io.constantin@gmail.com>
2024-12-28 16:17:22 +0200

    Update axi4Lite_driver.sv

--- Watchdog_timer/Watchdog_timer.srcs/sim_1/new/axi4Lite_driver.sv
+++ Watchdog_timer/Watchdog_timer.srcs/sim_1/new/axi4Lite_driver.sv
@@ -31,18 +31,17 @@ class axi4Lite_driver extends uvm_driver #(axi4Lite_transaction);
                 axi4Lite_interface.s_axi_awvalid = 1;
                 
                 //Wait until the consumer acknowledges the data and the address
+                `uvm_info("axi4Lite_driver", $psprintf("s_axi_awready: %0b, s_axi_wready: %0b", axi4Lite_interface.s_axi_awready, axi4Lite_interface.s_axi_wready), UVM_NONE)
                 wait(axi4Lite_interface.s_axi_awready == 1 && axi4Lite_interface.s_axi_wready == 1);
                 @(posedge axi4Lite_interface.s_axi_aclk);
                 axi4Lite_interface.s_axi_awvalid = 0;
 				axi4Lite_interface.s_axi_wvalid  = 0;
 
                 //Wait until the consumer aknowledges the write and check the response
+                `uvm_info("axi4Lite_driver", $psprintf("s_axi_bvalid: %0b", axi4Lite_interface.s_axi_bvalid), UVM_NONE)
                 wait(axi4Lite_interface.s_axi_bvalid == 1);
                 @(posedge axi4Lite_interface.s_axi_aclk);
-                 
-                axi4Lite_interface.s_axi_wvalid = 0;
-                axi4Lite_interface.s_axi_awvalid = 0;               
-              
+                              
                 if(axi4Lite_interface.s_axi_bresp == 2'b00) 
                     `uvm_info("axi4Lite_driver", "Write access successfull", UVM_NONE)
                 else
@@ -59,19 +58,21 @@ class axi4Lite_driver extends uvm_driver #(axi4Lite_transaction);
                 axi4Lite_interface.s_axi_arvalid = 1;
                 
                 // Wait until the consumer acknowledges the address
+                `uvm_info("axi4Lite_driver", $psprintf("s_axi_arready: %0b", axi4Lite_interface.s_axi_arready), UVM_NONE)
                 wait(axi4Lite_interface.s_axi_arready == 1);
                 `uvm_info("axi4Lite_driver", $psprintf("Entered second if statement"), UVM_NONE)
                 @(posedge axi4Lite_interface.s_axi_aclk);
                 axi4Lite_interface.s_axi_arvalid = 0;
                 
                 // Wait until the consumer signals that the read data is available on the bus
+               `uvm_info("axi4Lite_driver", $psprintf("s_axi_rvalid: %0b", axi4Lite_interface.s_axi_rvalid), UVM_NONE)
                 wait (axi4Lite_interface.s_axi_rvalid == 1);
                 
                 @(posedge axi4Lite_interface.s_axi_aclk);
                 if(axi4Lite_interface.s_axi_rresp == 2'b00) 
                     `uvm_info("axi4Lite_driver", "Read access successfull", UVM_NONE)
                 else
-                    `uvm_warning("axi4Lite_driver", $psprintf("The previous write generated %0b response", axi4Lite_interface.s_axi_rresp))
+                    `uvm_warning("axi4Lite_driver", $psprintf("The previous read generated %0b response", axi4Lite_interface.s_axi_rresp))
                 
                 // Acknowledge the response 
                 axi4Lite_interface.s_axi_rready = 1;
