d5b4b277ae0f6288fdb1a9791c79271ea9e18b82
Constantin Denis <denis.io.constantin@gmail.com>
2024-12-28 16:18:24 +0200

    Update environment.sv

--- Watchdog_timer/Watchdog_timer.srcs/sim_1/new/environment.sv
+++ Watchdog_timer/Watchdog_timer.srcs/sim_1/new/environment.sv
@@ -5,16 +5,19 @@ class environment extends uvm_env;
 	`uvm_component_utils(environment)
 
     axi4Lite_agent agent;
-    
+    scoreboard sb;
     
 	function new (string name = "env", uvm_component parent = null);
 		super.new(name, parent);
 	endfunction
 
 	virtual function void build_phase(uvm_phase phase);
-		agent = axi4Lite_agent::type_id::create("agent", this);
-		`uvm_info("DEBUG", "The build_phase of the environment was called", UVM_NONE)
+        agent = axi4Lite_agent::type_id::create("agent", this);
+		sb = scoreboard::type_id::create("sb", this);
 	endfunction
 	
-	
+	virtual function void connect_phase(uvm_phase phase);
+	    super.connect_phase(phase);
+	    agent.monitor.analysisPort.connect(sb.axi4Lite_imp_monitor);   
+	endfunction
 endclass : environment
