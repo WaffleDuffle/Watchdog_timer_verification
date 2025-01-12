d6c858a38665a51e868d146757b02d1e5b5534bd
Constantin Denis <denis.io.constantin@gmail.com>
2025-01-12 11:36:57 +0200

    Update axi4Lite_baseSequence.sv

--- Watchdog_timer/Watchdog_timer.srcs/sim_1/new/axi4Lite_baseSequence.sv
+++ Watchdog_timer/Watchdog_timer.srcs/sim_1/new/axi4Lite_baseSequence.sv
@@ -13,14 +13,14 @@ class axi4Lite_baseSequence extends uvm_sequence #(axi4Lite_transaction);
     
     virtual task body();
         
-        axi4Lite_item = axi4Lite_transaction::type_id::create("axi4Lite_item"); // numele il alegem noi, merge doar daca am uvm factory, line 5
+        axi4Lite_item = axi4Lite_transaction::type_id::create("axi4Lite_item"); // works only with uvm_factory
         
         `uvm_info("axi4Lite_baseSequence", $psprintf("Going to drive %d items", transactionAmount), UVM_NONE)
         
         repeat(transactionAmount) begin
             start_item(axi4Lite_item);             
             axi4Lite_item.randomize();        
-            finish_item(axi4Lite_item); //blocheaza executia pana cand am terminat cu secventele       
+            finish_item(axi4Lite_item); // wait for all sequences   
         end
         `uvm_info("axi4Lite_baseSequence", $psprintf("Finished generating axi4Lite transactions"), UVM_NONE)
     endtask : body
