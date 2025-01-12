a228f83fff5e360901fbe1eba9404bbf9f5c105a
Constantin Denis <denis.io.constantin@gmail.com>
2025-01-12 11:34:46 +0200

    Update testbench.sv, removed freeze and timebase_interrupt
    Activated Window WDT

--- Watchdog_timer/Watchdog_timer.srcs/sim_1/new/testbench.sv
+++ Watchdog_timer/Watchdog_timer.srcs/sim_1/new/testbench.sv
@@ -7,16 +7,15 @@ module testbench();
 
 // ----------- Signal declaration ----------- //
 // inputs
-    logic [3:0] s_axi_araddr ;   
+    logic [5:0] s_axi_araddr ;   
     logic s_axi_arvalid      ;
-    logic [3:0] s_axi_awaddr ;    
+    logic [5:0] s_axi_awaddr ;    
     logic s_axi_awvalid      ;
     logic s_axi_bready       ;   
     logic s_axi_rready       ;    
     logic [31:0] s_axi_wdata ;    
     logic [3:0] s_axi_wstrb  ;
     logic s_axi_wvalid       ;
-    logic freeze             ;
     logic s_axi_aclk         ;
     logic s_axi_aresetn      ;
     
@@ -30,7 +29,6 @@ module testbench();
     logic [1:0] s_axi_rresp  ;
     logic s_axi_rvalid       ;
     logic s_axi_wready       ;
-    logic timebase_interrupt ;
     logic wdt_interrupt      ;
     logic wdt_reset          ;
     
@@ -46,6 +44,7 @@ module testbench();
 
 initial begin
     s_axi_aclk = 0;
+    s_axi_wstrb = 5'hF;
     forever begin
         #5 s_axi_aclk = ~s_axi_aclk;
     end
@@ -90,4 +89,4 @@ initial begin
         end
     join_any 
 end
-endmodule                              
+endmodule      
